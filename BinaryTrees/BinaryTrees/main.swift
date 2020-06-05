cb12d480a4e18c6c972cb5b1f2f6703c4d7f19f0
Peter Stoehr <pstoehr@hof-university.de>
2020-06-05 15:53:38 +0200

    Initial commit

---
+++ BinaryTrees/BinaryTrees/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  BinaryTrees
+//
+
+import Foundation
+
+func createTree() -> BinaryNode<Int>
+{
+  let zero = BinaryNode(value: 0)
+  let one = BinaryNode(value: 1)
+  let five = BinaryNode(value: 5)
+  let seven = BinaryNode(value: 7)
+  let eight = BinaryNode(value: 8)
+  let nine = BinaryNode(value: 9)
+
+  seven.leftChild = one
+  one.leftChild = zero
+  one.rightChild = five
+  seven.rightChild = nine
+  nine.leftChild = eight
+  
+  return seven
+}
+
+let tree = createTree()
