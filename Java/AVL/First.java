45b4ca7caa64d370f1498c2ee09cb0034c1e9f7e
Peter Stoehr <pstoehr@hof-university.de>
2020-06-19 10:56:29 +0200

    Fixed Typo in First.java

--- Java/AVL/First.java
+++ Java/AVL/First.java
@@ -1,34 +1,7 @@
 public class First {
     
     public static void main(String args[])
-    {
-        /*
-        BST<Integer> l = new BST<Integer>();
-        for (int i=0; i<5; i++)
-            l.insert(i);
-        System.out.println(l);
-        
-        int values[] = {3,1,4,0,2};
-        BST<Integer> b = new BST<Integer>();
-        for (int v : values)
-            b.insert(v);
-        System.out.println(b);
-
-        BST<Integer> lr = new BST<Integer>();
-        for (int i=0; i<5; i++)
-            lr.insertRec(i);
-        System.out.println(l);
-        
-        BST<Integer> br = new BST<Integer>();
-        for (int v : values)
-            br.insertRec(v);
-        System.out.println(b);
-
-        System.out.println(b.isIn(42));
-        System.out.println(b.isIn(4));
-        */
-        System.out.println("<" + n.getValue() + ">");
-        
+    {        
         int complexContent[] = {50, 25, 12, 10, 17, 37, 32, 27, 33, 45, 75, 87};
         AVLTree<Integer> complexTree = new AVLTree<Integer>();
         for (int v : complexContent)
