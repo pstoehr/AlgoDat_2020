ad7ffa5e0c08880748ee98776ad82a3c2aef55be
Peter Stoehr <pstoehr@hof-university.de>
2020-06-19 11:49:18 +0200

    Anpassung für erste Programmierung

--- Java/AVL/First.java
+++ Java/AVL/First.java
@@ -2,12 +2,12 @@ public class First {
     
     public static void main(String args[])
     {        
-        int complexContent[] = {50, 25, 12, 10, 17, 37, 32, 27, 33, 45, 75, 87};
+        int complexContent[] = {50, 25, 75, 37, 40};
         AVLTree<Integer> complexTree = new AVLTree<Integer>();
         for (int v : complexContent)
             complexTree.insertRec(v);
         System.out.println(complexTree);
-        complexTree.delete(25);
+        complexTree.leftRotation(complexTree.root);
         System.out.println(complexTree);
     }
 }
