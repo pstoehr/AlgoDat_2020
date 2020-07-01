1b18bf73c758864dd7739894cfc1de8624747580
Peter Stoehr <pstoehr@hof-university.de>
2020-07-01 11:14:36 +0200

    Erste Version aus der Vorlesung

--- Java/Hashing/First.java
+++ Java/Hashing/First.java
@@ -2,10 +2,14 @@ public class First {
     
     public static void main(String args[])
     {
-        Hashing<String> h = new Hashing<String>(97);
+        Hashing<String> h = new Hashing<String>(53);
         Minions m = new Minions();
         
-        for (int i=0; i<42; i++)
-            h.add(m.getAny());
+        for (int i=0; i<m.count(); i++)
+            h.add(m.nameFor(i));
+        
+        System.out.println("Peter " + h.delete("Peter") + "false");
+        System.out.println("Kevin " + h.delete("Kevin") + "true");        
+
     }
 }
--- Java/Hashing/Hashing.java
+++ Java/Hashing/Hashing.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Hashing <T> {
+public class Hashing <T extends Comparable<? super T>> {
     private ArrayList<ArrayList<T>> hashTable;
     private int hashTableSize;
     
@@ -31,7 +31,7 @@ public class Hashing <T> {
             res += "[" + i + "]";
             res += "<";
             for (int j=0; j<collisonTable.size(); j += 1)
-                res += collisonTable.get(j);
+                res += collisonTable.get(j) + " ";
             res += ">\n";
         }
         return res;
@@ -39,11 +39,53 @@ public class Hashing <T> {
     
     public void add(T value)
     {
+        // Hash-Wert berechnen
+        int hash = value.hashCode();
+        int hashValue = Math.abs(hash) % hashTableSize;     // Position im Array
         
+        // Element in der Kollisionstabelle ablegen
+        ArrayList<T> c = hashTable.get(hashValue);
+        c.add(value);
     }
     
-    public Boolean isIn(T Value)
+    public Boolean isIn(T value)
     {
+        // Hash-Wert berechnen
+        int hash = value.hashCode();
+        int hashValue = Math.abs(hash) % hashTableSize;     // Position im Array
+        
+        // Kollisionstabelle suchen
+        ArrayList<T> c = hashTable.get(hashValue);
+        
+        for (T anEntry : c)
+        {
+            if (anEntry.compareTo(value) == 0) return true;
+        }
+        
         return false;
+        
+        // return c.contains(value);
+    }
+    
+    public Boolean delete(T value)
+    {
+        // Hash-Wert berechnen
+        int hash = value.hashCode();
+        int hashValue = Math.abs(hash) % hashTableSize;     // Position im Array
+        
+        // Kollisionstabelle suchen
+        ArrayList<T> c = hashTable.get(hashValue);
+        
+        for (T anEntry : c)
+        {
+            if (anEntry.compareTo(value) == 0) 
+            {
+                c.remove(value);
+                return true;
+            }
+        }
+        
+        return false;
+        
     }
 }
