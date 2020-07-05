788d3d45a2db907a41d37f19776dd0e29d3fccb2
Peter Stoehr <pstoehr@hof-university.de>
2020-07-05 18:07:23 +0200

    Update Example

--- Java/Hashing/First.java
+++ Java/Hashing/First.java
@@ -8,8 +8,6 @@ public class First {
         for (int i=0; i<m.count(); i++)
             h.add(m.nameFor(i));
         
-        System.out.println("Peter " + h.delete("Peter") + "false");
-        System.out.println("Kevin " + h.delete("Kevin") + "true");        
-
+        System.out.println(h);        
     }
 }
